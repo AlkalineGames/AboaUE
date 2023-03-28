fa7450fc462d8b773100ed0ea8812e08ab34155d
Christopher Augustus <repos@c4augustus.com>
2023-03-28 11:57:37 +0100

    create Source module AlkUemScheme

---
+++ Source/AlkUemScheme/AlkUemScheme.Build.cs
@@ -0,0 +1,18 @@
+// Copyright 2023 Alkaline Games, LLC.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+using UnrealBuildTool;
+
+public class AlkUemScheme : ModuleRules {
+  public AlkUemScheme(ReadOnlyTargetRules Target) : base(Target) {
+    bLegacyPublicIncludePaths = false;
+      // ^ !!! fixes VC error: command line is too long to fit in debug record
+    PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+    PrivateDependencyModuleNames.AddRange(new string[] {
+      "Core"
+    });
+  }
+}
