e5d9bc04eba4110f9138ef7990161c74f8294801
Christopher Augustus <repos@c4augustus.com>
2023-04-01 23:28:22 +0100

    enhance boot.scm with unimplemented functions that call ue-log for now

--- Source/scm/boot.scm
+++ Source/scm/boot.scm
@@ -6,4 +6,30 @@
 
 ;;; AlkalineScemeUE boot program
 
-(format #t "booting AlkalineSchemeUE")
+; TODO: @@@ NEEDS TO BE A LIST OF EXPRESSIONS FOR NOW
+(
+(ue-log "BEGIN booting AlkalineSchemeUE")
+
+(define (hook-ue-input device action durations func)
+  ; TODO: ### IMPLEMENT
+  (ue-log (format #f "called (hook-ue-input ~S ~S ~S ~S)" device action durations func)))
+
+(define (open-scheme-editor screen-pos)
+  ; TODO: ### IMPLEMENT
+  (ue-log (format #f "called (open-scheme-editor ~S)" screen-pos)))
+
+(define (print-ue text)
+  ; TODO: ### IMPLEMENT
+  (ue-log (format #f "called (print-ue ~S)" text)))
+
+(hook-ue-input 'pointing 'press '(200 0 200 0)
+  (lambda (screen-pos) (open-scheme-editor screen-pos)))
+
+(open-scheme-editor '(800 450))
+
+(print-ue "something")
+
+(ue-log "..END booting AlkalineSchemeUE")
+)
+; TODO: ### s7_eval_c_string(...) is reporting "syntax error"
+; here after the last expression successfully executes ?!?
