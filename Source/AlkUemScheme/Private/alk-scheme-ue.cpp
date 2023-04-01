5a5fac1bd6f7f8f923825ba3bcb34fb8159ffbc2
Christopher Augustus <repos@c4augustus.com>
2023-04-01 23:26:31 +0100

    create ue_log(...) s7 function and add it to s7 session in bootAlkSchemeUe()

--- Source/AlkUemScheme/Private/alk-scheme-ue.cpp
+++ Source/AlkUemScheme/Private/alk-scheme-ue.cpp
@@ -15,12 +15,28 @@
 DECLARE_LOG_CATEGORY_EXTERN(LogAlkalineScheme, Log, All);
 DEFINE_LOG_CATEGORY(LogAlkalineScheme);
 
+static s7_pointer
+ue_log(
+  s7_scheme* sc,
+  s7_pointer args
+) {
+  auto car = s7_car(args);
+  if (!s7_is_string(car))
+    return s7_wrong_type_arg_error(sc, "ue-log", 1, car, "a string");
+  UE_LOG(LogAlkalineScheme, Display, TEXT("%s"),
+    ANSI_TO_TCHAR(s7_string(car)));
+  return(s7_t(sc));
+}
+
 auto bootAlkSchemeUe() -> AlkSchemeUeMutant {
   auto s7session = s7_init();
   if (!s7session) {
     UE_LOG(LogAlkalineScheme, Error, TEXT("Failed to init s7 Scheme"))
     return {};
   }
+  s7_define_function(s7session,
+    "ue-log", ue_log, 1, 0, false, "(ue-log string)");
+
   FString scmPath = FPaths::ConvertRelativePathToFull(FPaths::Combine(
       FPaths::ProjectPluginsDir(),
       TEXT("AlkalineSchemeUE"), TEXT("Source"), TEXT("scm")));
@@ -38,12 +54,12 @@ auto bootAlkSchemeUe() -> AlkSchemeUeMutant {
       })
     ? "..END" : "FAILED")));
 #endif
-  FString code;
-  FString path = FPaths::Combine(scmPath, TEXT("boot.scm"));
-  if (!FFileHelper::LoadFileToString(code, *path, FFileHelper::EHashOptions::None))
+  FString source;
+  const FString path = FPaths::Combine(scmPath, TEXT("boot.scm"));
+  if (!FFileHelper::LoadFileToString(source, *path, FFileHelper::EHashOptions::None))
     UE_LOG(LogAlkalineScheme, Error, TEXT("Failed to read %s"), *path)
   else {
-    auto s7obj = s7_eval_c_string(s7session, TCHAR_TO_ANSI(*code));
+    auto s7obj = s7_eval_c_string(s7session, TCHAR_TO_ANSI(*source));
     UE_LOG(LogAlkalineScheme, Log, TEXT("Scheme session booted: %s"),
       ANSI_TO_TCHAR(s7_object_to_c_string(s7session, s7obj)));
   }
