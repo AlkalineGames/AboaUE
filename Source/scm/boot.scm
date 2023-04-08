d13a0b430c786e564338423a7e84235657402b05
Christopher Augustus <repos@c4augustus.com>
2023-04-08 17:34:40 +0100

    enhance boot.scm to trace input events

--- Source/scm/boot.scm
+++ Source/scm/boot.scm
@@ -6,42 +6,44 @@
 
 ;;; AlkalineScemeUE boot program
 
-; TODO: @@@ NEEDS TO BE A LIST OF EXPRESSIONS FOR NOW
-(
+( ;; TODO: @@@ NEEDS TO BE A LIST OF EXPRESSIONS FOR NOW
 (ue-log "TRACE SCHEME >>> BEGIN booting AlkalineSchemeUE")
 
 (define (hook-input device action durations func)
-  ; TODO: @@@ ASSUMING 'pointing DEVICE
-  (ue-log "TRACE SCHEME >>> BEGIN hook-input")
+  ;; TODO: @@@ ASSUMING 'pointing DEVICE
+  ;;(ue-log "TRACE SCHEME >>> BEGIN hook-input")
+  (define (trace-event event finger location)
+    (let ((info (format #f "TRACE SCHEME >>> ~S finger ~S at ~S" event finger location)))
+      (ue-print-string info)))
+      ;;(ue-log info)))
   (ue-bind-input-touch 'pressed
-    (lambda (finger location) (ue-print-string "pointing pressed")))
+    (lambda (finger location) (trace-event "pressed" finger location)))
   (ue-bind-input-touch 'released
-    (lambda (finger location) (ue-print-string "pointing released")))
+    (lambda (finger location) (trace-event "released" finger location)))
   (ue-bind-input-touch 'repeated
-    (lambda (finger location) (ue-print-string "pointing repeated")))
-  (open-scheme-editor '(800 450))
-  (ue-log "TRACE SCHEME <<< END hook-input"))
+    (lambda (finger location) (trace-event "repeated" finger location))))
+  ;;(ue-log "TRACE SCHEME <<< END hook-input"))
 
 (define (open-scheme-editor screen-pos)
-  ; TODO: ### IMPLEMENT
+  ;; TODO: ### IMPLEMENT
   (ue-log (format #f "TRACE SCHEME (open-scheme-editor ~S)" screen-pos)))
 
 (ue-hook-on-world-added
   (lambda (world)
-    (ue-log "TRACE SCHEME lambda from (ue-hook-on-world-added))")
+    ;;(ue-log "TRACE SCHEME lambda from (ue-hook-on-world-added))")
     (ue-hook-on-world-begin-play
       world
       (lambda (world)
-        (ue-log "TRACE SCHEME lambda from (ue-hook-on-world-begin-play)")
+        ;;(ue-log "TRACE SCHEME lambda from (ue-hook-on-world-begin-play)")
         (ue-print-string "Alkaline Scheme is alive")
         (hook-input 'pointing 'press '(200 0 200 0)
           (lambda (screen-pos) (open-scheme-editor screen-pos)))))))
 
-; TODO: ### NEED TO HOOK ON EXISTING WORLDS AS WELL
+;; TODO: ### NEED TO HOOK ON EXISTING WORLDS AS WELL
 
-;(ue-print-string "Alkaline Scheme is alive")
+;;(ue-print-string "Alkaline Scheme is alive")
 
 (ue-log "TRACE SCHEME <<< END booting AlkalineSchemeUE")
 )
-; TODO: ### s7_eval_c_string(...) is reporting "syntax error"
-; here after the last expression successfully executes ?!?
+;; TODO: ### s7_eval_c_string(...) is reporting "syntax error"
+;; here after the last expression successfully executes ?!?
