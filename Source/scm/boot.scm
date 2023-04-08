9dfe90d8c13a435d19b641b9a8103d0d6783129d
Christopher Augustus <repos@c4augustus.com>
2023-04-08 09:58:01 +0100

    enhance boot.scm to try to hook into input, but ue-hook-on-world-added is not actually fired by UE for newly added worlds

--- Source/scm/boot.scm
+++ Source/scm/boot.scm
@@ -8,24 +8,40 @@
 
 ; TODO: @@@ NEEDS TO BE A LIST OF EXPRESSIONS FOR NOW
 (
-(ue-log "BEGIN booting AlkalineSchemeUE")
+(ue-log "TRACE SCHEME >>> BEGIN booting AlkalineSchemeUE")
 
-(define (hook-ue-input device action durations func)
-  ; TODO: ### IMPLEMENT
-  (ue-log (format #f "called (hook-ue-input ~S ~S ~S ~S)" device action durations func)))
+(define (hook-input device action durations func)
+  ; TODO: @@@ ASSUMING 'pointing DEVICE
+  (ue-log "TRACE SCHEME >>> BEGIN hook-input")
+  (ue-bind-input-touch 'pressed
+    (lambda (finger location) (ue-print-string "pointing pressed")))
+  (ue-bind-input-touch 'released
+    (lambda (finger location) (ue-print-string "pointing released")))
+  (ue-bind-input-touch 'repeated
+    (lambda (finger location) (ue-print-string "pointing repeated")))
+  (open-scheme-editor '(800 450))
+  (ue-log "TRACE SCHEME <<< END hook-input"))
 
 (define (open-scheme-editor screen-pos)
   ; TODO: ### IMPLEMENT
-  (ue-log (format #f "called (open-scheme-editor ~S)" screen-pos)))
+  (ue-log (format #f "TRACE SCHEME (open-scheme-editor ~S)" screen-pos)))
 
-(hook-ue-input 'pointing 'press '(200 0 200 0)
-  (lambda (screen-pos) (open-scheme-editor screen-pos)))
+(ue-hook-on-world-added
+  (lambda (world)
+    (ue-log "TRACE SCHEME lambda from (ue-hook-on-world-added))")
+    (ue-hook-on-world-begin-play
+      world
+      (lambda (world)
+        (ue-log "TRACE SCHEME lambda from (ue-hook-on-world-begin-play)")
+        (ue-print-string "Alkaline Scheme is alive")
+        (hook-input 'pointing 'press '(200 0 200 0)
+          (lambda (screen-pos) (open-scheme-editor screen-pos)))))))
 
-(open-scheme-editor '(800 450))
+; TODO: ### NEED TO HOOK ON EXISTING WORLDS AS WELL
 
-(ue-print-string "Alkaline Scheme is alive.")
+;(ue-print-string "Alkaline Scheme is alive")
 
-(ue-log "..END booting AlkalineSchemeUE")
+(ue-log "TRACE SCHEME <<< END booting AlkalineSchemeUE")
 )
 ; TODO: ### s7_eval_c_string(...) is reporting "syntax error"
 ; here after the last expression successfully executes ?!?
