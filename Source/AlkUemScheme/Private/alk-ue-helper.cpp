362bc141fe8027535d35961d92cb1a5ffd2ee08d
Christopher Augustus <repos@c4augustus.com>
2023-04-08 09:52:14 +0100

    create alk-ue-helper functions

---
+++ Source/AlkUemScheme/Private/alk-ue-helper.cpp
@@ -0,0 +1,103 @@
+// Copyright 2023 Alkaline Games, LLC.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "alk-ue-helper.h"
+
+#include "Engine/Engine.h"
+#include "Components/InputComponent.h"
+#include "Kismet/GameplayStatics.h" // for GetPlayerPawn()
+
+DECLARE_LOG_CATEGORY_EXTERN(LogAlkUeHelper, Log, All);
+DEFINE_LOG_CATEGORY(LogAlkUeHelper);
+
+static auto EngineOrError(
+  char const * const purpose,
+  char const * const info
+) -> UEngine const * {
+  if (!GEngine)
+    UE_LOG(LogAlkUeHelper, Error,
+      TEXT("No Engine found for %s of %s"),
+      ANSI_TO_TCHAR(purpose),
+      ANSI_TO_TCHAR(info));
+  return GEngine;
+}
+
+static auto CurrentPlayWorld() -> UWorld const * {
+  return !GEngine ? nullptr : GEngine->GetCurrentPlayWorld();
+}
+
+static auto MutWorldContexts() -> TIndirectArray<FWorldContext> const * {
+  return !GEngine ? nullptr : &GEngine->GetWorldContexts();
+}
+
+static auto ApplyLambdaOnWorlds(
+  std::function<void (UWorld &)> const & lambda
+) -> void {
+  auto const * const contexts = MutWorldContexts();
+  if (contexts)
+    for (auto mutIter = contexts->begin(); mutIter != contexts->end(); ++mutIter) {
+      auto const mutWorld = (*mutIter).World();
+      if (mutWorld)
+        lambda(*mutWorld);
+    }
+}
+
+// TODO: @@@ DEPRECATE IF NOT USED
+static auto PrimaryWorldOrError(
+  char const * const purpose,
+  char const * const info
+) -> UWorld const * {
+  UWorld const * mutPrimary = nullptr;
+  auto const * const contexts = MutWorldContexts();
+  if (contexts)
+    for (auto mutIter = contexts->begin(); mutIter != contexts->end(); ++mutIter) {
+      auto const mutWorld = (*mutIter).World();
+      if (mutWorld) {
+        auto mutName = mutWorld->OriginalWorldName.ToString();
+        if (!mutPrimary && mutName != "None") {
+          mutPrimary = mutWorld;
+          mutName += " <- PRIMARY";
+        }
+        UE_LOG(LogAlkUeHelper, Display, TEXT("TRACE C++ found World %s"), *mutName);
+      }
+    }
+  if (!mutPrimary)
+    UE_LOG(LogAlkUeHelper, Error,
+      TEXT("No Engine/World found for %s of %s"),
+      ANSI_TO_TCHAR(purpose),
+      ANSI_TO_TCHAR(info));
+  return mutPrimary;
+}
+
+static auto FirstPlayerPawnOrError(
+  char const * const purpose,
+  char const * const info
+) -> APawn const * {
+  auto const * const world = PrimaryWorldOrError(purpose, info);
+  if (!world) { return nullptr; }
+  auto const * const pawn = UGameplayStatics::GetPlayerPawn(world, 0);
+  if (!pawn)
+    UE_LOG(LogAlkUeHelper, Error,
+      TEXT("No PlayerPawn found for %s of %s"),
+      ANSI_TO_TCHAR(purpose),
+      ANSI_TO_TCHAR(info));
+  return pawn;
+}
+
+static auto FirstInputComponentOrError(
+  char const * const purpose,
+  char const * const info
+) -> UInputComponent const * {
+  auto const * const pawn = FirstPlayerPawnOrError(purpose, info);
+  if (!pawn) { return nullptr; }
+  auto const * const input = pawn->InputComponent.Get();
+  if (!input)
+    UE_LOG(LogAlkUeHelper, Error,
+      TEXT("No InputComponent found for %s of %s"),
+      ANSI_TO_TCHAR(purpose),
+      ANSI_TO_TCHAR(info));
+  return input;
+}
---
+++ Source/AlkUemScheme/Public/alk-ue-helper.h
@@ -0,0 +1,69 @@
+// Copyright 2023 Alkaline Games, LLC.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+class APawn;
+class UInputComponent;
+class UWorld;
+
+static auto EngineOrError(
+  char const * const purpose,
+  char const * const info
+) -> UEngine const *;
+
+static auto MutEngineOrError(
+  char const * const purpose,
+  char const * const info
+) -> UEngine * {
+  return const_cast<UEngine*>(EngineOrError(purpose, info));
+}
+
+static auto CurrentPlayWorld()
+-> UWorld const *;
+
+static auto MutWorldContexts()
+-> TIndirectArray<FWorldContext> const *;
+
+static auto ApplyLambdaOnWorlds(
+  std::function<void (UWorld &)> const &
+) -> void;
+
+static auto PrimaryWorldOrError(
+  char const * const purpose,
+  char const * const info
+) -> UWorld const *;
+
+static auto MutPrimaryWorldOrError(
+  char const * const purpose,
+  char const * const info
+) -> UWorld * {
+  return const_cast<UWorld*>(PrimaryWorldOrError(purpose, info));
+}
+
+static auto FirstPlayerPawnOrError(
+  char const * const purpose,
+  char const * const info
+) -> APawn const *;
+
+static auto MutFirstPlayerPawnOrError(
+  char const * const purpose,
+  char const * const info
+) -> APawn * {
+  return const_cast<APawn*>(FirstPlayerPawnOrError(purpose, info));
+}
+
+static auto FirstInputComponentOrError(
+  char const * const purpose,
+  char const * const info
+) -> UInputComponent const *;
+
+static auto MutFirstInputComponentOrError(
+  char const * const purpose,
+  char const * const info
+) -> UInputComponent * {
+  return const_cast<UInputComponent*>(FirstInputComponentOrError(purpose, info));
+}
