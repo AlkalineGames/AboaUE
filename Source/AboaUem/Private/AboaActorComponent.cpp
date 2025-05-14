f6e671183233567ba9dd36190011ac1fbbb5db4e
Christopher Augustus <repos@c4augustus.com>
2025-05-14 20:35:37 +0000

    create AboaActorComponent so that aboa code can be added as a component to any actor

---
+++ Source/AboaUem/Private/AboaActorComponent.cpp
@@ -0,0 +1,92 @@
+// Copyright © 2025 Alkaline Games, LLC.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "AboaActorComponent.h"
+
+#include "EngineMinimal.h"
+
+#include "aboa-ue.h"
+#include "aboa-ue-helper.h"
+
+static auto pluginSourcePath(
+  FString const & dirPlugin,
+  FString const & dirSource,
+  FString const & filename
+) -> FString {
+  return PluginFilePath(dirPlugin, dirSource, filename);
+}
+
+UAboaActorComponent::UAboaActorComponent(
+  const FObjectInitializer& ObjectInitializer
+) : Super(ObjectInitializer)
+{
+  // contrary with super defaults
+  bWantsInitializeComponent                   = true;
+  PrimaryComponentTick.bCanEverTick           = true;
+  PrimaryComponentTick.bStartWithTickEnabled  = false;
+}
+
+// virtual
+UAboaActorComponent::~UAboaActorComponent()
+{}
+
+// virtual
+void UAboaActorComponent::InitializeComponent() {
+  Super::InitializeComponent();
+  auto results = runCachedAboaUeCodeAtPath(
+    pluginSourcePath(AboaDirPlugin, AboaDirSource, AboaFilename),
+    AboaNamespace + "-init",
+    makeAboaUeDataDict({
+      {"uobject", makeAboaUeDataUobject(*this)}}),
+    true); // forceReload TODO: ### UNTIL AUTO-RELOAD IS IMPLEMENTED
+  //PrintStringToScreen(dumpAboaUeDataDict(results));
+    // ^ TODO: ### TRACING
+  //PrintStringToScreen(stringFromAboaUeDataDict(results, "result"));
+    // ^ TODO: ### TRACING
+}
+
+// virtual
+void UAboaActorComponent::BeginPlay() {
+  Super::BeginPlay();
+  auto results = callLoadedAboaUeCode(
+    AboaNamespace + "-begin-play",
+    makeAboaUeDataDict({
+      {"uobject", makeAboaUeDataUobject(*this)}}));
+}
+
+// virtual
+void UAboaActorComponent::EndPlay(
+  const EEndPlayReason::Type EndPlayReason
+) {
+  Super::EndPlay(EndPlayReason);
+  auto results = callLoadedAboaUeCode(
+    AboaNamespace + "-end-play",
+    makeAboaUeDataDict({
+      {"uobject", makeAboaUeDataUobject(*this)}}));
+}
+
+// virtual
+void UAboaActorComponent::UninitializeComponent() {
+  Super::UninitializeComponent();
+  auto results = callLoadedAboaUeCode(
+    AboaNamespace + "-uninit",
+    makeAboaUeDataDict({
+      {"uobject", makeAboaUeDataUobject(*this)}}));
+}
+
+// virtual
+void UAboaActorComponent::TickComponent(
+  float                         DeltaTime,
+  enum ELevelTick               TickType,
+  FActorComponentTickFunction * ThisTickFunction
+) {
+  Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+  auto results = callLoadedAboaUeCode(
+    AboaNamespace + "-tick",
+    makeAboaUeDataDict({
+      {"uobject", makeAboaUeDataUobject(*this)},
+      {"delta",   makeAboaUeDataFloat(DeltaTime)}}));
+}
---
+++ Source/AboaUem/Public/AboaActorComponent.h
@@ -0,0 +1,40 @@
+// Copyright © 2025 Alkaline Games, LLC.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+
+#include "AboaActorComponent.generated.h"
+
+UCLASS(Blueprintable, meta=(ShortTooltip="aboa code for an actor"))
+class ABOAUEM_API UAboaActorComponent : public UActorComponent
+{
+  GENERATED_BODY()
+
+public:
+  UAboaActorComponent(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+  virtual ~UAboaActorComponent();
+
+  // blueprintables that provide the source file path
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AboaActorComponent)
+    FString AboaDirPlugin;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AboaActorComponent)
+    FString AboaDirSource;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AboaActorComponent)
+    FString AboaFilename;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AboaActorComponent)
+    FString AboaNamespace;
+
+  // UActorComponent overrides
+  virtual void InitializeComponent();
+  //virtual void ReadyForReplication(); TODO: do we need this?
+  virtual void BeginPlay();
+  virtual void EndPlay(const EEndPlayReason::Type EndPlayReason);
+  virtual void UninitializeComponent();
+  virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction);
+};
